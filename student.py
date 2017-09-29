2841425b105869ff909ffc679aab6967dbd6fab7
beckm33 <beckm@gilmour.org>
2017-09-29 14:28:02 -0400

    new changes to dance method

--- student.py
+++ student.py
@@ -58,6 +58,25 @@ class Piggy(pigo.Pigo):
         """executes a series of methods that add up to a compound dance"""
         print("\n---- LET'S DANCE ----\n")
         ##### WRITE YOUR FIRST PROJECT HERE
+        self.to_the_right()
+        self.to_the_left()
+        #self.now_kick()
+        #self.cha_cha()
+        #self.walk_it_by_yourself()
+
+    def to_the_right(self):
+        """Subroutine of dance method"""
+        for x in range(3):
+            self.encR(10)
+            self.encF(5)
+
+    def to_the_left(self):
+        for x in range(3):
+            self.encL(10)
+            self.encF(5)
+
+
+
 
     def nav(self):
         """auto pilots and attempts to maintain original heading"""
